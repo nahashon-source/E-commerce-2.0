0a313b4379b4c5933129e92536da5dd9c89e71cd
nahashon <nashonmwendwa0@gmail.com>
2024-12-03 13:09:23 +0300

    "Implement ProtectedRoute component to guard access to authenticated pages"

--- src/components/auth/ProtectedRoute.jsx
+++ src/components/auth/ProtectedRoute.jsx
@@ -2,13 +2,18 @@ import { useSelector } from 'react-redux';
 import { Navigate, useLocation } from 'react-router-dom';
 
 function ProtectedRoute({ children }) {
+  // Retrieve the token from Redux state to check if the user is authenticated
   const { token } = useSelector((state) => state.auth);
+  
+  // Get the current location to redirect the user back to the page they originally wanted to access
   const location = useLocation();
 
+// If there's no token (i.e., user is not authenticated), redirect to login page
   if (!token) {
     return <Navigate to="/login" state={{ from: location }} replace />;
   }
 
+  // If authenticated, render the protected child components
   return children;
 }
 
